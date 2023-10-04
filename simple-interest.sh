6197ad1cb408d82fb5c8b479edac23b3ac425565
ElliottRN <107939363+KielRN@users.noreply.github.com>
2023-10-04 15:23:09 -0500

    Update simple-interest.sh
    Updated Author

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,9 +1,9 @@
-   #!/bin/bash
+l  #!/bin/bash
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
+   # Additional Authors: KielRN
    # <your GitHub username>
    # Input:
    # p, principal amount
